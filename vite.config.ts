e40cc4ae3a22c8c1d6a215dc212602d6182310ab
jaanonim <mat8mro@gmail.com>
2025-12-30 14:15:49 +0100

    add tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,20 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
+
+const commonConfig = {
+    plugins: [react(), tailwindcss()],
+};
 
 export default defineConfig(({ command, mode }) => {
     if (command === "build") {
         const env = loadEnv(mode, process.cwd(), "");
         return {
             base: env.BASE_URL,
-            plugins: [react()],
+            ...commonConfig,
         };
     }
-    return {
-        plugins: [react()],
-    };
+    return { ...commonConfig };
 });
