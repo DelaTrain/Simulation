69b1e16b4ffbcd2cd20fdaa01ae0275a1a49113f
jaanonim <mat8mro@gmail.com>
2026-01-02 14:30:36 +0100

    Merge pull my finger request

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -64,4 +64,10 @@ export class Time {
         const ss = this.second.toString().padStart(2, "0");
         return `${hh}:${mm}:${ss}`;
     }
+
+    toShortString(): string {
+        const hh = this.hour.toString().padStart(2, "0");
+        const mm = this.minute.toString().padStart(2, "0");
+        return `${hh}:${mm}`;
+    }
 }
